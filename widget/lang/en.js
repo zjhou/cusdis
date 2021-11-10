0e94dd6fe9146aefb0d8e5a8bde75894768495cb
bbbottle <z@zjh.im>
2021-11-10 21:24:26 +0800

    fix(i18n): update i18n text

--- widget/lang/en.js
+++ widget/lang/en.js
@@ -11,10 +11,10 @@ export default {
   sending: 'sending...',
 
   // reply
-  mod_badge: 'MOD',
+  mod_badge: 'BBKi.ng',
   content_is_required: 'Content is required',
   nickname_is_required: 'Nickname is required',
 
   comment_has_been_sent:
-    'Your comment has been sent. Please wait for approval.',
+    'Your comment has been sent. Please wait for checking 18+ content.',
 }
